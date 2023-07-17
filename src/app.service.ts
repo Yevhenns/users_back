5dba6e5368387370860642f973c36a355239b950
Yevhenns <gifestum@gmail.com>
2023-07-17 10:07:08 +0300

    pg

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return [
+      { id: 1, name: 'anna', age: '21' },
+      { id: 2, name: 'hanna', age: '22' }
+    ]
   }
 }
