10876cdbb3c2b22a288ce994cf0236459bfc6f06
Yevhenns <gifestum@gmail.com>
2023-07-18 21:48:56 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
-// import { UsersModule } from './users/users.module'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const port = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule)
+  app.enableCors()
   await app.listen(port, () => console.log(`started on port ${port}`))
 }
 bootstrap()
