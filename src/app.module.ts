4320c05aba26a22c327aa265d445f4b6e55a01e3
Yevhenns <gifestum@gmail.com>
2023-07-18 13:47:15 +0300

    str

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { User } from './users/users.model'
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      password: String(process.env.POSTGRES_PASSWORD),
       database: process.env.POSTGRES_DB,
       models: [User],
       autoLoadModels: true
